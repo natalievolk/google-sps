2a0aad59bed0d982e4c1b07591baf7af03ef5004
Natalie Volk <natalievolk@rogers.com>
2022-06-14 20:53:17 +0000

    Add contact info page

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -13,15 +13,17 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
+    String nameValue = request.getParameter("name-input");
+    String emailValue = request.getParameter("email-input");
     String textValue = request.getParameter("text-input");
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("Name: " + nameValue + "\nEmail: " + emailValue + "\nMessage: " + textValue);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println("Name: " + nameValue + "\nEmail: " + emailValue + "\nMessage: " + textValue);
 
     // Redirect the user to stay on the home page
-    response.sendRedirect("/");
+    response.sendRedirect("/contact-me.html");
   }
 }
